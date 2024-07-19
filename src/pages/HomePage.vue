81c75f1092ac4e2500bb940834b869956cf57635
Evan Varozza <VarozzaEJ@gmail.com>
2024-07-19 10:38:43 -0600

    first commit

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>The Network</h1>
+</template>
+
+<style scoped lang="scss">
+.home {
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+
+  .home-card {
+    width: clamp(500px, 50vw, 100%);
+
+    >img {
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
+}
+</style>
