6dc680ed868f8084b8d0c1652fd3e7d6b0ce1b3a
Evan Varozza <VarozzaEJ@gmail.com>
2024-07-19 15:11:28 -0600

    styled post card more

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -19,19 +19,23 @@ const props = defineProps({ postProp: { type: Post, required: true } })
                     <div class="d-flex flex-column justify-content-between">
                         <div class="card-body">
                             <h5 class="card-title"></h5>
-                            <p class="card-text">{{ postProp.body }}</p>
+                            <p class="card-text fs-5">{{ postProp.body }}</p>
                             <p class="card-text"><small class="text-body-secondary">{{
                                 postProp.createdAt.toLocaleString()
                                     }}</small></p>
                         </div>
                         <div class="card-body mt-5">
-                            <RouterLink :to="{ name: 'Profile', params: { profileId: postProp.creatorId } }">
-                                <img class="img-fluid creator-img" :src="postProp.creator.picture"
-                                    :alt="postProp.creator.name">
-                            </RouterLink>
-                            <!-- TODO like button is here -->
-                            <h5 class="text-end"><i class="mdi mdi-heart text-end fs-2" role="button"
-                                    title="Like This Post"></i></h5>
+                            <div class="d-flex align-items-center justify-content-end mb-4 ">
+                                <p class="card-text fs-4 fw-bold me-2">{{ postProp.creator.name }}</p>
+                                <RouterLink :to="{ name: 'Profile', params: { profileId: postProp.creatorId } }">
+                                    <img class="img-fluid creator-img" :src="postProp.creator.picture"
+                                        :alt="postProp.creator.name">
+                                </RouterLink>
+                                <!-- TODO like button is here -->
+                            </div>
+                            <h5 class=" d-flex align-items-center justify-content-end"><i
+                                    class="mdi mdi-heart text-end fs-2" role="button" title="Like This Post"></i>{{
+                                        postProp.likeIds.length }}</h5>
                         </div>
                     </div>
                 </div>
